715813ab44f56189705c05e486f90bbe4646d816
Devon Germano <devon.m.germano@gmail.com>
2023-10-11 00:51:36 -0700

    Test

--- src/redis-pub-sub.service.ts
+++ src/redis-pub-sub.service.ts
@@ -6,8 +6,8 @@ export class RedisPubSubService extends RedisPubSub implements OnModuleInit {
   constructor() {
     super({
       connection: {
-        host: process.env.REDIS_HOST || 'localhost',
-        port: Number(process.env.REDIS_PORT) || 6379,
+        host: process.env.REDISHOST || 'localhost',
+        port: Number(process.env.REDISPORT) || 6379,
         username: process.env.REDIS_USERNAME || undefined,
         password: process.env.REDIS_PASSWORD || undefined,
         db: Number(process.env.REDIS_DATABASE) || 0,
